0dfa32f53daa82cbab1e0f17aaf23b5b6abf47d1
Sanyam <iit2019129@gmail.com>
2021-08-06 18:06:12 +0530

    navbar added

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import './Nav.css'
+function Nav() {
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 70) {
+                setShow(true);
+            } else {
+                setShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener('scroll');
+        };
+    }, []);
+
+    return (
+        <div className={`nav ${show && 'nav__black'}`}>
+            <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix logo" />
+            <img className="nav__avatar" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png" alt="" />
+        </div >
+    )
+}
+
+export default Nav
